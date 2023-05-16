c2b2e8c225fa9b92ad1ba8ffdf13ad8b41bc0a16
Peter Banda <peter.banda@protonmail.com>
2023-05-16 12:25:31 +0200

    Handy fun 'getMetadata(key: String)' introduce for Match data holder.

--- pinecone-core/src/main/scala/io/cequence/pineconescala/domain/response/QueryResponse.scala
+++ pinecone-core/src/main/scala/io/cequence/pineconescala/domain/response/QueryResponse.scala
@@ -12,7 +12,10 @@ case class QueryResponse(
 case class Match(
   id: String,
   score: Double,
-  values: Seq[Double], // not returned when includeValues = false
+  values: Seq[Double],                   // not returned when includeValues = false
   sparseValues: Option[SparseVector],
-  metadata: Option[Map[String, String]] // not returned when includeMetadata = true
-)
+  metadata: Option[Map[String, String]]  // not returned when includeMetadata = false
+) {
+  val metadataUnwrapped : Map[String, String] = metadata.getOrElse(Map[String, String]())
+  def getMetadata(key: String): Option[String] = metadataUnwrapped.get(key)
+}
