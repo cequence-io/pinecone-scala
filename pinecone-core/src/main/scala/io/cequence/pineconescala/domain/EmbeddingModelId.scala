a6a1f7158f4034d18a060f037891626427b637f3
Peter Banda <peter.banda@protonmail.com>
2025-01-03 10:50:57 +0100

    New embedding models - pinecone-sparse-english-v0, new reranking models - cohere-rerank-3.5, pinecone-rerank-v0

--- pinecone-core/src/main/scala/io/cequence/pineconescala/domain/EmbeddingModelId.scala
+++ pinecone-core/src/main/scala/io/cequence/pineconescala/domain/EmbeddingModelId.scala
@@ -1,5 +1,8 @@
 package io.cequence.pineconescala.domain
 
 object EmbeddingModelId {
+  // 507 input tokens, dim 1024 (dense)
   val multilingual_e5_large = "multilingual-e5-large"
+  // 512 input tokens
+  val pinecone_sparse_english_v0 = "pinecone-sparse-english-v0"
 }
--- pinecone-core/src/main/scala/io/cequence/pineconescala/domain/RerankModelId.scala
+++ pinecone-core/src/main/scala/io/cequence/pineconescala/domain/RerankModelId.scala
@@ -1,5 +1,10 @@
 package io.cequence.pineconescala.domain
 
 object RerankModelId {
+  // 1024 input tokens
   val bge_reranker_v2_m3 = "bge-reranker-v2-m3"
+  // 4096 input tokens
+  val cohere_rerank_3_5 = "cohere-rerank-3.5"
+  // 512 input tokens
+  val pinecone_rerank_v0 = "pinecone-rerank-v0"
 }
